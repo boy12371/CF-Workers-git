0c0b2ff80d89e56e7cb0e78f64ec92899845c29a
Richard Kuang <boy12371@hotmail.com>
2024-06-20 17:38:55 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,34 +1,21 @@
 const API_HOST = "github.com";
 
 async function handleRequest(request, ctx) {
-  const url = new URL(request.url);
-  const { pathname, search, hash } = url;
-  const destinationURL = "https://" + API_HOST + pathname + search + hash;
+    const url = new URL(request.url);
+    const { pathname, search, hash } = url;
+    const destinationURL = "https://" + API_HOST + pathname + search + hash;
 
-  let response = await caches.default.match(request);
-  if (!response) {
-    response = await fetch(destinationURL);
-    ctx.waitUntil(caches.default.put(request, response.clone()));
-  }
+    let response = await caches.default.match(request);
+    if (!response) {
+        response = await fetch(destinationURL);
+        ctx.waitUntil(caches.default.put(request, response.clone()));
+    }
 
-  return response;
+    return response;
 }
 
 export default {
-  async fetch(request, env, ctx) {
-    return handleRequest(request, ctx);
-  },
-
-  // Updated CSP configuration
-  async setHeaders(event) {
-    const { request } = event;
-    const headers = new Headers();
-	headers.delete('content-security-policy')
-	headers.delete('content-security-policy-report-only')
-	headers.delete('clear-site-data')
-    // headers.set('Content-Security-Policy', `script-src 'self' *.pages.dev *.sveil.com github.githubassets.com;`);
-    return new Response(null, {
-      headers,
-    });
-  },
+    async fetch(request, env, ctx) {
+        return handleRequest(request, ctx);
+    }
 };
