9cdc8adfdcdadd6e4a4b9e8482560eae96fd3a5b
Richard Kuang <boy12371@hotmail.com>
2024-06-20 17:11:24 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,20 +1,31 @@
-const API_HOST = "github.com"
+const API_HOST = "github.com";
 
 async function handleRequest(request, ctx) {
-    const url = new URL(request.url)
-    const { pathname, search, hash } = url
-    const destinationURL = "https://" + API_HOST + pathname + search + hash
-    let response = await caches.default.match(request)
-    if (!response) {
-        response = await fetch(destinationURL)
-        ctx.waitUntil(caches.default.put(request, response.clone()))
-    }
+  const url = new URL(request.url);
+  const { pathname, search, hash } = url;
+  const destinationURL = "https://" + API_HOST + pathname + search + hash;
 
-    return response
+  let response = await caches.default.match(request);
+  if (!response) {
+    response = await fetch(destinationURL);
+    ctx.waitUntil(caches.default.put(request, response.clone()));
+  }
+
+  return response;
 }
 
 export default {
-    async fetch(request, env, ctx) {
-        return handleRequest(request, ctx);
-    }
+  async fetch(request, env, ctx) {
+    return handleRequest(request, ctx);
+  },
+
+  // Updated CSP configuration
+  async setHeaders(event) {
+    const { request } = event;
+    const headers = new Headers();
+    headers.set('Content-Security-Policy', `script-src 'self' https://git.sveil.com https://github.githubassets.com;`);
+    return new Response(null, {
+      headers,
+    });
+  },
 };
