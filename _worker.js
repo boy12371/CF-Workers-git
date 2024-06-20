6485bed72c28061c6cc6dd8f5f7459ef10884d29
Richard Kuang <boy12371@hotmail.com>
2024-06-20 16:43:02 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,35 +1,15 @@
 const API_HOST = "github.com"
 
-async function handleRequest(request, ctx) {
-  const url = new URL(request.url)
-  const pathname = url.pathname
-  const search = url.search
-  const pathWithParams = pathname + search
-
-  if (pathname.startsWith("/static/")) {
-      return retrieveStatic(request, pathWithParams, ctx)
-  } else {
-      return forwardRequest(request, pathWithParams)
-  }
-}
-
-async function retrieveStatic(request, pathname, ctx) {
-  let response = await caches.default.match(request)
-  if (!response) {
-      response = await fetch(`https://${API_HOST}${pathname}`)
-      ctx.waitUntil(caches.default.put(request, response.clone()))
-  }
-  return response
-}
-
-async function forwardRequest(request, pathWithSearch) {
-  const originRequest = new Request(request)
-  originRequest.headers.delete("cookie")
-  return await fetch(`https://${API_HOST}${pathWithSearch}`, originRequest)
+const statusCode = 301
+async function handleRequest(request) {
+    const url = new URL(request.url)
+    const { pathname, search, hash } = url
+    const destinationURL = "https://" + API_HOST + pathname + search + hash
+    return Response.redirect(destinationURL, statusCode)
 }
 
 export default {
-  async fetch(request, env, ctx) {
-    return handleRequest(request, ctx);
-  }
+    async fetch(request, env, ctx) {
+        return handleRequest(request, ctx);
+    }
 };
